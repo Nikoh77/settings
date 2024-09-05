888a29a71dd4beb4546d7c8df25e583dc0241efa
Nikoh <nikoh@nikoh.it>
2024-09-05 23:33:31 +0200

    Bump version to 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="settings",
-    version="1.0.5",
+    version="1.0.6",
     description="A settings manager package",
     packages=find_packages(),
     author="Nikoh",
